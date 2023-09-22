c8e71ce5b83592ef340079d7c4c5e920fe9f5eb5
MathieuDionne2032450 <112105657+MathieuDionne2032450@users.noreply.github.com>
2023-09-22 16:47:04 -0400

    configuration cach

--- config/database.php
+++ config/database.php
@@ -130,20 +130,20 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('AZURE_REDIS_PASSWORD'),
-            'port' => env('AZURE_REDIS_PORT', '6379'),
-            'database' => env('AZURE_REDIS_DATABASE', '0'),
-        ],
-
-        'cache' => [
-            'scheme' => 'tls',
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'database' => env('REDIS_DATABASE', '0'),
+        ],
+
+        'cache' => [
+            'scheme' => 'tls',
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('AZURE_REDIS_PASSWORD'),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
+            'database' => env('AZURE_REDIS_CACHE_DB', '1'),
         ],
 
     ],
